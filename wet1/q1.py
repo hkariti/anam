d53a6d8badaef93d25c1850b43cd348eddd5b1cf
Hagai Kariti <hkariti@gmail.com>
2019-01-02 11:51:15 +0200

    q1.2

--- wet1/q1.py
+++ wet1/q1.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import scipy.io as sio
 
 def generate_swiss_roll(size=3000):
     u = np.random.random(size)*10
@@ -22,5 +24,21 @@ def plot_swiss_roll(x, y, z, color):
         ax.scatter(x, y, z, c=color)
         plt.show()
 
-swiss_roll = generate_swiss_roll()
-plot_swiss_roll(*swiss_roll)
+def load_face_data(filename='./face_data.mat'):
+    data = sio.loadmat(filename)
+    return data['images']
+
+def plot_face_data(images):
+    indices = [1, 400, 532]
+    for i in indices:
+        image = images[:, i].reshape(64,64).T
+        plt.imshow(image, cmap='gray')
+        plt.title('Face #{}'.format(i))
+        plt.show()
+
+if __name__ == '__main__':
+    swiss_roll = generate_swiss_roll()
+    plot_swiss_roll(*swiss_roll)
+
+    images = load_face_data()
+    plot_face_data(images)
