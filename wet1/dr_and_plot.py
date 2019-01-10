b9a09d6f739695482735ac0e5d675223ffa4e4d5
Hagai Kariti <hkariti@gmail.com>
2019-01-10 11:42:24 +0200

    Make plot functions generic

--- wet1/dr_and_plot.py
+++ wet1/dr_and_plot.py
@@ -4,13 +4,13 @@ import pca_functions
 import manifold_functions
 import datasets
 
-def swiss_pca_and_plot(data, labels=None):
+def pca_and_plot(data, data_name, labels=None):
     pca_reduced = pca_functions.pca(data)
     kpca_reduced = pca_functions.kpca(data)
     manual_reduced = pca_functions.manual_pca(data)
 
     fig = plt.figure()
-    fig.suptitle("2D Representation of Swiss Roll", fontsize=16)
+    fig.suptitle("2D Representation of {}".format(data_name), fontsize=16)
     ax = plt.subplot(1, 3, 1)
     ax.scatter(pca_reduced[:, 0], pca_reduced[:, 1], c=labels)
     ax.set_title("Linear PCA")
@@ -24,12 +24,12 @@ def swiss_pca_and_plot(data, labels=None):
     ax.set_title("PCA with Manual Features")
     plt.show()
 
-def swiss_manifold_and_plot(data, labels=None):
+def manifold_and_plot(data, data_name, labels=None):
     isomap_reduced = manifold_functions.isomap(data, neighbors=20)
     lle_reduced = manifold_functions.lle(data, neighbors=30)
 
     fig = plt.figure()
-    fig.suptitle("2D Representation of Swiss Roll Using Manifold Methods", fontsize=16)
+    fig.suptitle("2D Representation of {} Using Manifold Methods".format(data_name), fontsize=16)
     ax = plt.subplot(1, 2, 1)
     ax.scatter(isomap_reduced[:, 0], isomap_reduced[:, 1], c=labels)
     ax.set_title("Isomap")
@@ -39,7 +39,10 @@ def swiss_manifold_and_plot(data, labels=None):
     ax.set_title("LLE")
     plt.show()
 
-if __name__ == '__main__':
+def do_swiss_roll():
     swiss_roll, colors = datasets.generate_swiss_roll()
-    swiss_pca_and_plot(swiss_roll, colors)
-    swiss_manifold_and_plot(swiss_roll, colors)
+    pca_and_plot(swiss_roll, 'Swiss Roll', colors)
+    manifold_and_plot(swiss_roll, 'Swiss Roll', colors)
+
+if __name__ == '__main__':
+    do_swiss_roll()
